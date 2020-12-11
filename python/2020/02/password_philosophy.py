f06945d8f7bf5680a9112296f07ccdcefd4f8c69
Dave Maynard <davemaynard24@gmail.com>
2020-12-10 23:03:03 -0500

    solved part 2 of Day 2 - Password Philosophy

--- python/2020/02/password_philosophy.py
+++ python/2020/02/password_philosophy.py
@@ -37,3 +37,36 @@ def password_philosophy(l):
     if char_count >= min_count and char_count <= max_count:
       valid_count += 1
   return valid_count
+
+
+# --- Part Two ---
+# While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+# The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+# Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+# Given the same example list from above:
+
+# 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+# 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+# 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+# How many passwords are valid according to the new interpretation of the policies?
+
+
+def password_philosophy_part_2(l):
+  l = l.split('\n')
+
+  valid_count = 0
+  for item in l:
+    dash_index = item.index('-')
+    i = int(item[:dash_index])
+    space_index = item.index(' ', dash_index)
+    j = int(item[dash_index + 1:space_index])
+    char = item[space_index + 1]
+    password = item[space_index + 4:]
+    if (password[i - 1] == char
+        and password[j - 1] != char) or (password[i - 1] != char
+                                         and password[j - 1] == char):
+      valid_count += 1
+  return valid_count
