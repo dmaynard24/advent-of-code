9f17155e0acbab1959de37e405992d596b8e2a01
Dave Maynard <davemaynard24@gmail.com>
2020-12-13 16:57:13 -0500

    solved part 2 of Day 5: Binary Boarding

--- python/2020/05/binary_boarding.py
+++ python/2020/05/binary_boarding.py
@@ -5,11 +5,11 @@
 
 # Instead of zones or groups, this airline uses binary space partitioning to seat people. A seat might be specified like FBFBBFFRLR, where F means "front", B means "back", L means "left", and R means "right".
 
-# The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane (numbered 0 through 127). Each letter tells you which half of a region the given seat is in. Start with the whole list of rows; the first letter indicates whether the seat is in the front (0 through 63) or the back (64 through 127). The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row.
+# The first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane (numbered 0 through ROW_COUNT - 1). Each letter tells you which half of a region the given seat is in. Start with the whole list of rows; the first letter indicates whether the seat is in the front (0 through 63) or the back (64 through ROW_COUNT - 1). The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row.
 
 # For example, consider just the first seven characters of FBFBBFFRLR:
 
-# Start by considering the whole range, rows 0 through 127.
+# Start by considering the whole range, rows 0 through ROW_COUNT - 1.
 # F means to take the lower half, keeping rows 0 through 63.
 # B means to take the upper half, keeping rows 32 through 63.
 # F means to take the lower half, keeping rows 32 through 47.
@@ -38,15 +38,21 @@
 
 import math, sys
 
+ROW_COUNT = 128
+COL_COUNT = 8
 
-def get_binary_partition_result(chars, upper):
+get_id = lambda row, col: row * 8 + col
+
+
+def get_binary_partition_result(chars, char_index, upper, lower_flag):
   lower = 0
-  for i in range(len(chars)):
-    char = chars[i]
-    if char == 'F' or char == 'L':
+  while lower != upper:
+    char = chars[char_index]
+    if char == lower_flag:
       upper -= math.ceil((upper - lower) / 2)
     else:
       lower += math.ceil((upper - lower) / 2)
+    char_index += 1
   return lower
 
 
@@ -55,7 +61,34 @@ def binary_boarding(boarding_passes):
 
   max_id = -sys.maxsize
   for boarding_pass in boarding_passes:
-    row = get_binary_partition_result(boarding_pass[:7], 127)
-    col = get_binary_partition_result(boarding_pass[7:], 7)
-    max_id = max(max_id, row * 8 + col)
+    row = get_binary_partition_result(boarding_pass, 0, ROW_COUNT - 1, 'F')
+    col = get_binary_partition_result(boarding_pass, 7, COL_COUNT - 1, 'L')
+    seat_id = get_id(row, col)
+    max_id = max(max_id, seat_id)
   return max_id
+
+
+# --- Part Two ---
+# Ding! The "fasten seat belt" signs have turned on. Time to find your seat.
+
+# It's a completely full flight, so your seat should be the only missing boarding pass in your list. However, there's a catch: some of the seats at the very front and back of the plane don't exist on this aircraft, so they'll be missing from your list as well.
+
+# Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1 from yours will be in your list.
+
+# What is the ID of your seat?
+
+
+def binary_boarding_part_2(boarding_passes):
+  boarding_passes = boarding_passes.split('\n')
+  available_seat_ids = {get_id(row, col) for col in range(COL_COUNT) for row in range(ROW_COUNT)}
+
+  for boarding_pass in boarding_passes:
+    row = get_binary_partition_result(boarding_pass, 0, ROW_COUNT - 1, 'F')
+    col = get_binary_partition_result(boarding_pass, 7, COL_COUNT - 1, 'L')
+    available_seat_ids.discard(get_id(row, col))
+
+  available_seat_ids_arr = list(available_seat_ids)
+  for i in range(1, len(available_seat_ids_arr)):
+    seat_id = available_seat_ids_arr[i]
+    if available_seat_ids_arr[i - 1] != seat_id - 1 and available_seat_ids_arr[i + 1] != seat_id + 1:
+      return seat_id
